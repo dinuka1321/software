715497c4a8b379fc2333ab68f046c23d6e2f2f29
dinuka1321 <www.dinukanirmal358@gmail.com>
2025-08-24 21:50:45 +0530

    i have finished task 03(module02)

---
+++ module02/task03.py
@@ -0,0 +1,8 @@
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+
+Perimeter = float((length+width)*2)
+Area = float(length*width)
+
+print(f" Perimeter is : { Perimeter: 6.2f}")
+print(f"Area is : {Area: 6.2f}")
