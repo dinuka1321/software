6f51595482a8c02844013392b3c75e939e0c6a5d
dinuka1321 <www.dinukanirmal358@gmail.com>
2025-08-31 16:43:45 +0530

    i have finished task01(module03)

--- module03/task 01.py
+++ module03/task 01.py
@@ -3,5 +3,5 @@ difference = 0
 if size>=42:
     print("zander proves the limit, you can keep it ")
 else:
-    difference = float(42 - size)
+    difference = float( 42 - size )
     print(f"the zander is {difference:4.2f} cm below the limit")
