8467833f172bbcf6691ad7613fd126cf199c30ef
dinuka1321 <www.dinukanirmal358@gmail.com>
2025-09-01 00:15:05 +0530

    i have finished task 05(module 04)

---
+++ module04/task 06.py
@@ -0,0 +1,17 @@
+import random
+
+N = int(input("Random points: "))
+n = 0
+i = 0
+
+while i < N:
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+
+    if x ** 2 + y ** 2 < 1:
+        n += 1
+
+    i += 1
+
+pi_approximate = 4 * n / N
+print("Approximation of pi:", pi_approximate)
