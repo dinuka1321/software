8be8d0fc6bfe6aabd826c04a0574b8be63bd73f7
dinuka1321 <www.dinukanirmal358@gmail.com>
2025-09-07 22:38:24 +0530

    i have finished task 04(module 06)

---
+++ module06/task 04.py
@@ -0,0 +1,10 @@
+def tot(numbers):
+    return sum(numbers)
+
+def main():
+    nums = [2, 5, 7, 10, 4]
+    result = tot(nums)
+    print("List:", nums)
+    print("Sum of list:", result)
+
+main()
