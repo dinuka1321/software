10691c31431e129f223fac0bb66720676e13c67c
dinuka1321 <www.dinukanirmal358@gmail.com>
2025-10-27 01:29:32 +0530

    i have finished task 03(module 09)

---
+++ module09/task 03.py
@@ -0,0 +1,22 @@
+class Car:
+    def __init__(self, registration_number, maximum_speed, current_speed=0, travelled_distance=0):
+        self.registration_number = registration_number
+        self.maximum_speed = maximum_speed
+        self.current_speed = current_speed
+        self.travelled_distance = travelled_distance
+
+    def acceleration(self, speed_change):
+        self.current_speed += speed_change
+        if self.current_speed > self.maximum_speed:
+            self.current_speed = self.maximum_speed
+        elif self.current_speed < 0:
+            self.current_speed = 0
+
+    def drive(self, hours):
+        self.travelled_distance += self.current_speed * hours
+
+
+car1 = Car("ABC-123", 142)
+car1.acceleration(60)
+car1.drive(1.5)
+print(f"Travelled distance: {car1.travelled_distance} km")
